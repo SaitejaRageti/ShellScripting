daedb40c0cc4b33bcd802f3b2ef5030413c5ac8f
saitejarageti <saitejarageti129@gmail.com>
2025-05-19 20:23:49 +0530

    hello world

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+
+echo "Anusha :: Hi Teja, I Need to tell you one thing"
+echo "Teja :: Hi Anu, what happend? Please tell me"
+echo "Anusha :: I am feeling so sad as I didnt get seat in MPT counsling"
+echo "Teja :: Dont worry Anu!! We have 2nd phase. we shall give one more try. Be positive"
+echo "Anusha :: Okay Teja.Thank you!"
