44b2f38d67bc3de469ecad04664f9a5f6f30134a
saitejarageti <saitejarageti129@gmail.com>
2025-05-19 20:41:35 +0530

    variables

---
+++ 03.variables.sh
@@ -0,0 +1,12 @@
+#####################################################################
+#### Learming abou variables ##############
+
+person1="Anusha"
+person2="Teja"
+
+echo "$person1 :: Hi $person2, I Need to tell you one thing"
+echo "$person2 :: Hi Anu, what happend? Please tell me"
+echo "$person1 :: I am feeling so sad as I didnt get seat in MPT counsling"
+echo "$person2 :: Dont worry Anu!! We have 2nd phase. we shall give one more try. Be positive"
+echo "$person1 :: Okay $person2.Thank you!"
+
