0735553305f0ba56434e86d4b360835d9af10e49
saitejarageti <saitejarageti129@gmail.com>
2025-05-29 12:33:31 +0530

    hello world

--- 09.install_commands.sh
+++ 09.install_commands.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo " you are running this script with non root user. Please run with root user"
     exit 1  ##GIVE OTHER THAN 0 UPTO 127
@@ -12,11 +12,11 @@ fi
 
 dnf list installed mysql 
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
     echo " mysql installion is not done yet...going to install"
     dnf install mysql -y
-    if [ $? -eq 0]
+    if [ $? -eq 0 ]
     then
         echo "My sql is installed sucessfully: SUCCESS"
     else
