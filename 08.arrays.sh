f4fb109d8c6b20f78cfa3bdbab10f76d8e2c6666
saitejarageti <saitejarageti129@gmail.com>
2025-05-29 11:46:50 +0530

    hello world

--- 08.arrays.sh
+++ 08.arrays.sh
@@ -5,4 +5,6 @@ myfamily=("Gangadhar" "Laxmi" "Saiteja" "Anusha")
 Main_member=${myfamily[0]}
 family=${myfamily[@]}
 echo "$family"
+meandwife=${myfamily[0,3]}
 echo "$Main_member"
+echo "$meandwife"
