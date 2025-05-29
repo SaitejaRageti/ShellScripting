e405f563806d1fbcb7d8b302b4d023cb5cc8607c
saitejarageti <saitejarageti129@gmail.com>
2025-05-29 11:02:32 +0530

    hello world

---
+++ 07.conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+number1=$1
+number2=$2
+
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+echo "starting my script at : $timestamp"
+Sum=$(($1+$2))
+echo "$Sum"
+if [ $Sum -gt 60 ]
+then
+    echo "$Sum id greater than 60"
+elif [ $Sum -eq 60 ]
+    echo "$Sum is equal to 60"
+else
+    echo "its a small number"
+fi 
