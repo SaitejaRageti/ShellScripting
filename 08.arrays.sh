9b04c8025bde57dd044306d9feee339f84262df8
saitejarageti <saitejarageti129@gmail.com>
2025-05-29 12:11:57 +0530

    hello world

--- 08.arrays.sh
+++ 08.arrays.sh
@@ -6,3 +6,13 @@ Main_member=${myfamily[0]}
 family=${myfamily[@]}
 echo "$family"
 echo "$Main_member"
+
+echo "All variables passed to the script: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
