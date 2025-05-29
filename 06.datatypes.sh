88d98a49b084d6b3e32f2b7da88f4f1092465402
saitejarageti <saitejarageti129@gmail.com>
2025-05-29 10:47:13 +0530

    hello world

--- 06.datatypes.sh
+++ 06.datatypes.sh
@@ -4,10 +4,10 @@ number1=25
 number2=35
 timestamp=$(date)
 echo "starting my script on $timestamp"
-Sum = $(($number1+$number2))
-echo "$sum"
-if [$sum -ge 60];then
-    echo "$sum id greater than 60"
+Sum=$(($number1+$number2))
+echo "$Sum"
+if [$Sum -ge 60];then
+    echo "$Sum id greater than 60"
 else
     echo "its a small number"
 fi 
