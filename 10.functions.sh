0dc2ceaaa11c8c07312df1a17a6467c86975a6db
saitejarageti <saitejarageti129@gmail.com>
2025-05-30 12:55:06 +0530

    hello world

---
+++ 10.functions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo " you are running this script with non root user. Please run with root user"
+    exit 1  ##GIVE OTHER THAN 0 UPTO 127
+else
+    echo "you are running with ROOT user....GO AHEAD"
+fi
+
+validate(){
+    if [ $1 -eq 0 ]then;
+        echo "installing $2 is success"
+    else
+        echo "installing $2 is failure"
+        exit
+    fi
+
+}
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]then;
+    echo " mysql is installed already"
+    exit
+else
+    dnf install mysql -y
+    validate $? mysql
+fi 
+
