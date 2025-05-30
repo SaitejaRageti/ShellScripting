15dfa6f402d12d8c2b00198c2217b68c3e4a48fc
saitejarageti <saitejarageti129@gmail.com>
2025-05-30 14:20:09 +0530

    hello world

--- 10.functions.sh
+++ 10.functions.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
+script_start_time=$(date)
+script-name=$(basename "$0" .sh)
+LOGS_FOLDER="/var/logs/shell_scripting_logs"
+mkdir -p $LOGS_FOLDER
+Logfile=$LOGS_FOLDER/"${script-name}.log"
 
 if [ $USERID -ne 0 ]
 then
@@ -21,11 +26,11 @@ validate(){
 
 }
 
-dnf list installed mysql
+dnf list installed mysql & >> $Logfile | tee -a $LOG_FILE
 
 if [ $? -eq 0 ]
 then
-    echo " mysql is installed already"
+    echo "mysql is installed already"
     
 else
     echo "mysql is not installed yet going to install"
@@ -33,18 +38,18 @@ else
     validate $? "mysql"
 fi 
 
-dnf list installed python3
+dnf list installed python3 & >> $Logfile | tee -a $LOG_FILE
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    echo "python is installed already"
-else
-    echo "python is not installed yet going to install"
+    echo "python is not installed yet going to install" | tee -a $LOG_FILE
     dnf install python3 -y
     validate $? "python"
+else
+    echo "python is already installed nothing to do" | tee -a $LOG_FILE
 fi
 
-dnf list installed nginx
+dnf list installed nginx & >> | tee -a $LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "nginx is not installed... going to install it"
